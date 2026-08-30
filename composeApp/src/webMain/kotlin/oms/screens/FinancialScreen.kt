ab4db4c8a36ed7bf5bc7feb9b0844dc7defb778d
Karatanov <karatanov@gmail.com>
2026-08-30 17:36:51 +0300

    fix(finance): use ScrollState animation member

--- composeApp/src/webMain/kotlin/oms/screens/FinancialScreen.kt
+++ composeApp/src/webMain/kotlin/oms/screens/FinancialScreen.kt
@@ -1,6 +1,5 @@
 package oms.screens
 
-import androidx.compose.foundation.animateScrollTo
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.horizontalScroll
